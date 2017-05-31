f6c5d0aee5c90192fd9e4fe2e1e24f252b571fe7
bashimon <imon.bashir@haw-hamburg.de>
2017-05-31 23:06:17 +0200

    Gästebuch mit Datenbank verbunden

--- app/src/main/java/com/seamuseum/auswahlelement/guestbook/EntriesActivity.java
+++ app/src/main/java/com/seamuseum/auswahlelement/guestbook/EntriesActivity.java
@@ -1,11 +1,7 @@
 package com.seamuseum.auswahlelement.guestbook;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.seamuseum.auswahlelement.R;
 
@@ -14,6 +10,6 @@ public class EntriesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_entries);
+        setContentView(R.layout.activity_guestbook_entries);
     }
 }
--- app/src/main/java/com/seamuseum/auswahlelement/guestbook/WriteEntryActivity.java
+++ app/src/main/java/com/seamuseum/auswahlelement/guestbook/WriteEntryActivity.java
@@ -3,29 +3,36 @@ package com.seamuseum.auswahlelement.guestbook;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.seamuseum.auswahlelement.R;
+
+import java.util.Date;
 
 public class WriteEntryActivity extends Activity {
 
     public Button bttn;
     public EditText textfeld;
     public EditText namefeld;
-    private List<String>  autor, inhalt;
+    private DatabaseReference _rootRef;
+    private DatabaseReference _childRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        autor = new ArrayList<String>();
-        inhalt = new ArrayList<String>();
-        setContentView(R.layout.activity_guestbook);
+        setContentView(R.layout.activity_guestbook_writeentry);
+        textfeld = (EditText) findViewById(R.id.textfeld);
+        namefeld = (EditText) findViewById(R.id.namefeld);
+        _rootRef = FirebaseDatabase.getInstance().getReference();
         bttn = (Button) findViewById(R.id.button4);
         bttn.setOnClickListener(new View.OnClickListener()
         {
@@ -34,22 +41,24 @@ public class WriteEntryActivity extends Activity {
             {
                 Context context = getApplicationContext();
                 int duration = Toast.LENGTH_LONG;
-                autor.add(namefeld.getText().toString());
-                inhalt.add(textfeld.getText().toString());
-                String text="";
-                for(int i = 0; i < autor.size(); ++i)
-                {
-                    text+= autor.get(i) + ": " + inhalt.get(i) + "\n";
-                }
-
-                Toast toast = Toast.makeText(context, text, duration);
+                long msTime = System.currentTimeMillis();
+                //Android Studio supportet alles über Java 7 nicht vollumfänglich, u.a. LocalDate.
+                Date curDateTime = new Date(msTime);
+                _childRef = _rootRef.child("guestbookentries").child(namefeld.getText().toString() + " @ " + curDateTime);
+                _childRef.setValue(textfeld.getText().toString());
+                Toast toast = Toast.makeText(context, "Abgesendet", duration);
                 toast.show();
             }
         });
-        textfeld = (EditText) findViewById(R.id.textfeld);
-        namefeld = (EditText) findViewById(R.id.namefeld);
         //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         // setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
     }
 
 }
