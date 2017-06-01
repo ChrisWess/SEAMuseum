653493718ae0ace9d0ec9949d9384de86d5a5d12
ElhamE <elham.esmat@haw-hamburg.de>
2017-06-01 10:57:51 +0200

    Gästebuchkomponente  überarbeitet

--- app/src/main/java/com/seamuseum/auswahlelement/guestbook/EntriesActivity.java
+++ app/src/main/java/com/seamuseum/auswahlelement/guestbook/EntriesActivity.java
@@ -1,11 +1,11 @@
 package com.seamuseum.auswahlelement.guestbook;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.seamuseum.auswahlelement.R;
 
-public class EntriesActivity extends AppCompatActivity {
+public class EntriesActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/seamuseum/auswahlelement/guestbook/GuestbookActivity.java
+++ app/src/main/java/com/seamuseum/auswahlelement/guestbook/GuestbookActivity.java
@@ -1,14 +1,14 @@
 package com.seamuseum.auswahlelement.guestbook;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.seamuseum.auswahlelement.R;
 
-public class GuestbookActivity extends AppCompatActivity {
+public class GuestbookActivity extends Activity {
 
     public Button lesen;
     public Button verfassen;
